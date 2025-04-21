4d09aa90783c1e74afbb24095879fca7170249d3
Debora <ds336756@gmail.com>
2025-04-21 02:47:29 -0300

    Criando a Controller do aluno

---
+++ EscolaEcommerce/Controllers/AlunoController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EscolaEcommerce.Models;
+using EscolaEcommerce.Repositorio;
+
+namespace EscolaEcommerce.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private readonly AlunoRepositorio _alunoRepositorio;
+        public AlunoController(AlunoRepositorio alunoRepositorio)
+        {
+            _alunoRepositorio = alunoRepositorio;
+        }
+
+        public IActionResult Principal()
+        {
+            return View(_alunoRepositorio.TodosAlunos());
+        }
+
+        public IActionResult CadastrarAluno()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarAluno(Aluno aluno)
+        {
+            _alunoRepositorio.CadastrarAluno(aluno);
+            return RedirectToAction(nameof(Principal));
+        }
+
+        public IActionResult EditarAluno(int id)
+        {
+            var aluno = _alunoRepositorio.ObterAluno(id);
+
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditarAluno(int id, [Bind("Rm, Nome, Email, Idade")] Aluno aluno)
+        {
+            if (id != aluno.Rm)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (_alunoRepositorio.Atualizar(aluno))
+                    {
+                        return RedirectToAction(nameof(Principal));
+                    }
+                }
+
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Erro ao editar.");
+                    return View(aluno);
+                }
+            }
+
+            return View(aluno);
+        }
+
+        public IActionResult ExcluirAluno(int id)
+        {
+            _alunoRepositorio.Excluir(id);
+            return RedirectToAction(nameof(Principal));
+        }
+    }
+}
