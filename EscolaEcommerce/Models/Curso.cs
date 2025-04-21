87cf8028b1b1641b5f0a76a80f51fc46d050e0f4
Debora <ds336756@gmail.com>
2025-04-20 23:46:19 -0300

    Criando a model do curso

---
+++ EscolaEcommerce/Models/Curso.cs
@@ -0,0 +1,10 @@
+﻿namespace EscolaEcommerce.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        List<Curso>? ListaCurso { get; set; }
+    }
+}
