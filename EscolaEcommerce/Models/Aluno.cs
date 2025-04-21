710e9119bee021dae4ba662e5bc9728ea00912d5
Debora <ds336756@gmail.com>
2025-04-20 23:38:31 -0300

    Criando a model do Aluno

---
+++ EscolaEcommerce/Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace EscolaEcommerce.Models
+{
+    public class Aluno
+    {
+        public int Rm { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public int Idade { get; set; }
+        List<Aluno>? ListaAluno { get; set; }
+    }
+}
