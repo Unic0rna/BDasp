34ae242bd7be2fbb0da1bb1094056e072391b6fd
Debora <ds336756@gmail.com>
2025-04-21 02:07:15 -0300

    Adicionando a injeção de dependencia

--- EscolaEcommerce/Program.cs
+++ EscolaEcommerce/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<AlunoRepositorio>();
+builder.Services.AddScoped<CursoRepositorio>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
