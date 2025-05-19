d97233e6ab5c79c4c20e89fce9fdf298aebeffb7
Debora <ds336756@gmail.com>
2025-05-19 00:49:54 -0300

    Aula

--- script.sql
+++ script.sql
@@ -16,17 +16,26 @@ Nome varchar(50) not null,
 Descricao varchar(100) not null
 );
 
+create table Aluno_Curso
+(
+Id int primary key auto_increment,
+IdCurso int,
+IdAluno int,
+Nome varchar(50) not null,
+Descricao varchar(100) not null,
+foreign key (IdCurso) references Curso(Id),
+foreign key (IdAluno) references Aluno(Rm)
+);
+
+
 delimiter $$
-create procedure insertAluno (vNome varchar(50), vEmail varchar(100), vIdade int, out retorno int)
+create procedure insertAluno (vNome varchar(50), vEmail varchar(100), vIdade int)
 begin
-
-
 
 if not exists (select Rm from Aluno where Nome = vNome) then
 insert into Aluno(Nome, Email, Idade) values (vNome, vEmail, vIdade);
+update Aluno set Idade = Idade + 10;
 
-else
-set retorno = 1;
 end if;
 end
 $$
